7d820c4803c1b59e4bee0ee2aebc26a6e91ec99c
root <root@HTPC.localdomain>
2020-04-12 14:26:49 +1000

    feat: device operation

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # py-opensprinkler
-Python module for OpenSprinker API
+
+Python module for OpenSprinker API. Tested against OpenSprinkler version 2.1.9.
+
+## Usage
+
+```python
+from pyopensprinkler import OpenSprinkler
+
+os = OpenSprinkler("hostname:port", "md5password")
+os.device.getFirmwareVersion()
+```
+
+## Commands
+
+### Device
+
+`os.device.getFirmwareVersion()`
+
+`os.device.getHardwareVersion()`
+
+`os.device.getLastRun()`
+
+`os.device.getRainDelay()`
+
+`os.device.getRainDelayStopTime()`
+
+`os.device.getRainSensor1()`
+
+`os.device.getRainSensor2()`
+
+`os.device.getRainSensorLegacy()`
+Rain sensor for firmware version <= 2.1.7
+
+`os.device.getOperationEnabled()`
+
+`os.device.enable()`
+
+`os.device.disable()`
--- pyopensprinkler/device.py
+++ pyopensprinkler/device.py
@@ -13,6 +13,18 @@ class Device(object):
         (resp, content) = self.opensprinkler.get('jo')
         return content[option]
 
+    def _getVariable(self, option):
+        """Retrieve option"""
+        (resp, content) = self.opensprinkler.get('jc')
+        return content[option]
+
+    def _setVariable(self, option, value):
+        """Retrieve option"""
+        params = {}
+        params[option] = value
+        (resp, content) = self.opensprinkler.get('cv', params)
+        return content['result']
+
     def getFirmwareVersion(self):
         """Retrieve firmware version"""
         return self._getOption('fwv')
@@ -20,3 +32,39 @@ class Device(object):
     def getHardwareVersion(self):
         """Retrieve hardware version"""
         return self._getOption('hwv')
+
+    def getLastRun(self):
+        """Retrieve hardware version"""
+        return self._getVariable('lrun')
+
+    def getRainDelay(self):
+        """Retrieve rain delay"""
+        return self._getVariable('rd')
+
+    def getRainDelayStopTime(self):
+        """Retrieve rain delay stop time"""
+        return self._getVariable('rdst')
+
+    def getRainSensor1(self):
+        """Retrieve hardware version"""
+        return self._getVariable('sn1')
+
+    def getRainSensor2(self):
+        """Retrieve hardware version"""
+        return self._getVariable('sn2')
+
+    def getRainSensorLegacy(self):
+        """Retrieve hardware version"""
+        return self._getVariable('rs')
+
+    def getOperationEnabled(self):
+        """Retrieve operation enabled"""
+        return self._getVariable('en')
+
+    def enable(self):
+        """Enable operation"""
+        return self._setVariable('en', 1)
+
+    def disable(self):
+        """Disable operation"""
+        return self._setVariable('en', 0)
