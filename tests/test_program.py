03b467c7d9246131803f9a7c495ab630ad3b5eee
Nymus Booysen <20920039+booysenn@users.noreply.github.com>
2024-09-19 19:06:17 +0000

    Add support to use weather adjustment when running a program

--- tests/test_program.py
+++ tests/test_program.py
@@ -1,4 +1,7 @@
+import asyncio
+
 import pytest
+from const import FIRMWARE_VERSION
 
 
 @pytest.fixture
@@ -31,6 +34,72 @@ class TestProgram:
         await program.set_odd_even_restriction(0)
         assert program.odd_even_restriction == 0
         assert program.odd_even_restriction_name is None
+
+    @pytest.mark.skipif(
+        FIRMWARE_VERSION <= 216, reason="only for version 217 and above"
+    )
+    @pytest.mark.asyncio
+    async def test_program_manual_run(self, controller, program):
+        await program.set_station_duration(0, 25)
+        # controller.set_water_level only works from version 219
+        if FIRMWARE_VERSION >= 219:
+            await controller.set_water_level(20)
+        else:
+            await controller._set_option("o23", 20)
+
+        await program._manual_run()
+        await asyncio.sleep(1)
+        await controller.refresh()
+        assert (
+            controller.stations[0].end_time - controller.stations[0].start_time
+        ) == 25
+
+        await program._manual_run(1)
+        await asyncio.sleep(1)
+        await controller.refresh()
+        assert (
+            controller.stations[0].end_time - controller.stations[0].start_time
+        ) == 5
+
+        await program._manual_run(0)
+        await asyncio.sleep(1)
+        await controller.refresh()
+        assert (
+            controller.stations[0].end_time - controller.stations[0].start_time
+        ) == 25
+
+    @pytest.mark.skipif(
+        FIRMWARE_VERSION <= 216, reason="only for version 217 and above"
+    )
+    @pytest.mark.asyncio
+    async def test_program_run(self, controller, program):
+        await program.set_station_duration(0, 25)
+        # controller.set_water_level only works from version 219
+        if FIRMWARE_VERSION >= 219:
+            await controller.set_water_level(20)
+        else:
+            await controller._set_option("o23", 20)
+
+        await program.run()
+        await asyncio.sleep(1)
+        await controller.refresh()
+        assert (
+            controller.stations[0].end_time - controller.stations[0].start_time
+        ) == 25
+
+        await program.run(1)
+        await asyncio.sleep(1)
+        await controller.refresh()
+        assert (
+            controller.stations[0].end_time - controller.stations[0].start_time
+        ) == 5
+
+        await program.run(0)
+        await asyncio.sleep(1)
+        await controller.refresh()
+        assert (
+            controller.stations[0].end_time - controller.stations[0].start_time
+        ) == 25
 
     @pytest.mark.asyncio
     async def test_program_schedule_type(self, controller, program):
